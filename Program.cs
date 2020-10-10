d4f27101de0fd7b20f4a394409f6f6c90150b52b
KellyCristinaAires <k.woys@hotmail.com>
2020-10-10 18:23:26 -0300

    atividade numeral extenso

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Humanizer;
+
+namespace  testeResp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            string texto;
+
+            Console.Write("Digite um numero: ");
+            numero = int.Parse(Console.ReadLine ());
+
+            texto = numero.ToWords();
+            Console.Write("Seu número em texto é: ");
+            Console.WriteLine(texto);
+        }
+    }
+}
